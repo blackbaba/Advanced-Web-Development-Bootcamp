57d7bcd3d7f6fc9ecf0ed68aeb1d6eb183b83f0e
Black Baba <amrinder.dhindsa1@gmail.com>
2019-09-23 11:16:13 +0530

    Add basic layout for Todo SPA

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 var todoRoutes = require('./routes/todos');
 
 // GET Root route
 app.get("/", function (req, res) {
-  res.send("Hi from Root Route");
+  res.sendfile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
