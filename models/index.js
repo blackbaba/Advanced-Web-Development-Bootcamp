af34814975dbff6ef7dc2ff5a3ff3a7ed5309bd8
Black Baba <amrinder.dhindsa1@gmail.com>
2019-09-24 14:48:46 +0530

    Complete Todo App

--- models/index.js
+++ models/index.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api'); //Create a new database if not existing - Running on Port 27017
+var mongoURL = 'mongodb://heroku_3bbsf6r1:eqjgbih3bf4ks21nrh6tfrs2p7@ds159641.mlab.com:59641/heroku_3bbsf6r1'; //'mongodb://localhost/todo-api';
+mongoose.connect(mongoURL, function (err, db) {
+  console.log(mongoURL);
+  if (err) {
+    console.log("There has been a database connection error ...");
+  } else {
+    console.log("Connected to MongoDB successfully ..");
+  }
+}); //Create a new database if not existing - Running on Port 27017
 
 mongoose.Promise = Promise;
 module.exports.Todo = require("./todo");
