f892a775f2cd9e236ed9a5b1e1c8d26ff296dd36
Black Baba <amrinder.dhindsa1@gmail.com>
2019-10-10 16:38:04 +0530

    Create React frontend

--- warbler-client/src/store/reducers/errors.js
+++ warbler-client/src/store/reducers/errors.js
@@ -1,6 +1,5 @@
 import { ADD_ERROR, REMOVE_ERROR } from "../actionTypes";
 
-
 export default (state = { message: null }, action) => {
   switch (action.type) {
     case ADD_ERROR:
--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -1,12 +1,10 @@
-require("dotenv").load;
-require("dotenv").config();
-
+require("dotenv").load();
 var jwt = require("jsonwebtoken");
 
-exports.loginRequired = function (req, res, next) {
+exports.loginRequired = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
       if (decoded) {
         next();
       } else {
@@ -18,10 +16,10 @@ exports.loginRequired = function (req, res, next) {
   }
 };
 
-exports.ensureCorrectUser = function (req, res, next) {
+exports.ensureCorrectUser = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
       if (decoded && decoded.id === req.params.id) {
         return next();
       } else {
