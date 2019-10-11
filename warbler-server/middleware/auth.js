cce740585c903970ce8ef52d46f4dcd21bccaf08
Black Baba <amrinder.dhindsa1@gmail.com>
2019-10-11 18:41:51 +0530

    Deploy SPA to Production

--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -1,10 +1,10 @@
-require("dotenv").load();
+require("dotenv").config();
 var jwt = require("jsonwebtoken");
 
-exports.loginRequired = function(req, res, next) {
+exports.loginRequired = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
       if (decoded) {
         next();
       } else {
@@ -16,10 +16,10 @@ exports.loginRequired = function(req, res, next) {
   }
 };
 
-exports.ensureCorrectUser = function(req, res, next) {
+exports.ensureCorrectUser = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
       if (decoded && decoded.id === req.params.id) {
         return next();
       } else {
